cea4e03e7768e4ed4786ff04baeb4214df660aa2
Bartosz <bartoszchmielecki2003@gmail.com>
2024-05-23 18:25:04 +0200

    Login i registration web page

---
+++ Login Maj2024/script.js
@@ -0,0 +1,13 @@
+const signUpButton=document.getElementById('signUpButton');
+const signInButton=document.getElementById('signInButton');
+const signInForm=document.getElementById('signIn');
+const signUpForm=document.getElementById('signup');
+//nie moze byc spacji przed i po = w document.getElementById//
+signUpButton.addEventListener('click',function(){
+    signInForm.style.display="none";
+    signUpForm.style.display="block";
+})
+signInButton.addEventListener('click', function(){
+    signInForm.style.display="block";
+    signUpForm.style.display="none";
+})
